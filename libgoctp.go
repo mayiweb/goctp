13ee9c7af36c637f88db498bce04353e6b31568d
Nathan Yang <nathan.yang@usask.ca>
2021-03-09 14:56:28 +0800

    refactor cgo config and delete cgo config for windows

--- libgoctp.go
+++ libgoctp.go
@@ -1,12 +1,7 @@
-
 package goctp
 
 /*
-#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/v6.5.1_20200908_api_tradeapi_se_linux64 -Wl,-rpath,${SRCDIR}/api/v6.5.1_20200908_api_tradeapi_se_linux64 -lthostmduserapi_se -lthosttraderapi_se -lstdc++
-#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/v6.5.1_20200908_api_tradeapi_se_linux64
-
-// windows 不可用，留着记录
-#cgo windows LDFLAGS: -fPIC -L${SRCDIR}/api/v6.5.1_20200908_tradeapi64_se_windows -Wl,-rpath,${SRCDIR}/api/v6.5.1_20200908_tradeapi64_se_windows ${SRCDIR}/api/v6.5.1_20200908_tradeapi64_se_windows/thostmduserapi_se.lib ${SRCDIR}/api/v6.5.1_20200908_tradeapi64_se_windows/thosttraderapi_se.lib -lthostmduserapi_se -lthosttraderapi_se
-#cgo windows CPPFLAGS: -fPIC -I${SRCDIR}/api/v6.5.1_20200908_tradeapi64_se_windows -DISLIB -DWIN32 -DLIB_MD_API_EXPORT -DLIB_TRADER_API_EXPORT
+#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api -Wl,-rpath,${SRCDIR}/api -lthostmduserapi_se -lthosttraderapi_se -lstdc++
+#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api
 */
 import "C"
