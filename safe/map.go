127358154b273db57c2405f92b081c48518cac0d
Faye Long <feilong.xiong@foxmail.com>
2023-12-25 17:49:52 +0800

    up map.go

--- safe/map.go
+++ safe/map.go
@@ -1,5 +1,5 @@
 /**
- * 安全 Map，防止并发读写
+ * 安全 Map 防止并发读写
  */
 
 package safe
@@ -36,8 +36,8 @@ func (p *Map) Set(name interface{}, value interface{}) {
 // 获取 map
 func (p *Map) Get(name interface{}) (interface{}, bool) {
 
-    p.Lock.Lock()
-    defer p.Lock.Unlock()
+    p.Lock.RLock()
+    defer p.Lock.RUnlock()
 
     mVal, mOk := p.Map[name]
     if !mOk {
@@ -67,20 +67,25 @@ func (p *Map) Clear() {
 
 // 数据条数
 func (p *Map) Size() int {
-    p.Lock.Lock()
-    defer p.Lock.Unlock()
+    p.Lock.RLock()
+    defer p.Lock.RUnlock()
 
     return len(p.Map)
 }
 
 // 获取全部 map
 func (p *Map) GetAll() map[interface{}]interface{} {
-    p.Lock.Lock()
-    defer p.Lock.Unlock()
+    p.Lock.RLock()
+    defer p.Lock.RUnlock()
 
-    list := p.Map
+    // 申明一个新的 map 将 p.map 数据写入新 map 中，再访问数据就不是同一份数据了
+    Result := make(map[interface{}]interface{}, 0)
 
-    return list
+    for k, v := range p.Map {
+        Result[k] = v
+    }
+
+    return Result
 }
 
 // ------------------- 二级 map -------------------
@@ -105,8 +110,8 @@ func (p *Map2) Set(key1 interface{}, key2 interface{}, value interface{}) {
 // 获取 map
 func (p *Map2) Get(key1 interface{}, key2 interface{}) (interface{}, bool) {
 
-    p.Lock.Lock()
-    defer p.Lock.Unlock()
+    p.Lock.RLock()
+    defer p.Lock.RUnlock()
 
     mVal, mOk := p.Map[key1][key2]
     if !mOk {
@@ -118,21 +123,28 @@ func (p *Map2) Get(key1 interface{}, key2 interface{}) (interface{}, bool) {
 
 // 获取二级 key 列表
 func (p *Map2) GetList(key1 interface{}) (map[interface{}]interface{}, bool) {
-    p.Lock.Lock()
-    defer p.Lock.Unlock()
+    p.Lock.RLock()
+    defer p.Lock.RUnlock()
 
-    mVal, mOk := p.Map[key1]
+    mLists, mOk := p.Map[key1]
     if !mOk {
         return nil, false
     }
 
-    return mVal, true
+    // 申明一个新的 map 将 p.map 数据写入新 map 中，再访问数据就不是同一份数据了
+    Result := make(map[interface{}]interface{}, 0)
+
+    for k, v := range mLists {
+        Result[k] = v
+    }
+
+    return Result, true
 }
 
 // 获取二级 key 列数据条数
 func (p *Map2) GetListSize(key1 interface{}) int {
-    p.Lock.Lock()
-    defer p.Lock.Unlock()
+    p.Lock.RLock()
+    defer p.Lock.RUnlock()
 
     _, mOk := p.Map[key1]
     if !mOk {
@@ -171,16 +183,30 @@ func (p *Map2) Clear() {
 
 // 数据条数
 func (p *Map2) Size() int {
-    p.Lock.Lock()
-    defer p.Lock.Unlock()
+    p.Lock.RLock()
+    defer p.Lock.RUnlock()
 
     return len(p.Map)
 }
 
 // 获取全部 map
 func (p *Map2) GetAll() map[interface{}]map[interface{}]interface{} {
-    p.Lock.Lock()
-    defer p.Lock.Unlock()
+    p.Lock.RLock()
+    defer p.Lock.RUnlock()
 
-    return p.Map
+    // 申明一个新的 map 将 p.map 数据写入新 map 中，再访问数据就不是同一份数据了
+    Result := make(map[interface{}]map[interface{}]interface{}, 0)
+
+    for k, v := range p.Map {
+
+        ResultList := make(map[interface{}]interface{}, 0)
+
+        for sk, sv := range v {
+            ResultList[sk] = sv
+        }
+
+        Result[k] = ResultList
+    }
+
+    return Result
 }
